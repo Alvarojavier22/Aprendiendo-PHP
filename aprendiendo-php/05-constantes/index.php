e7cc378a04779ab936d7b88af9be27d03a4d03d5
Alvaro Chagas <alvaro.chagas@estudiantes.utec.edu.uy>
2025-08-12 16:23:03 -0300

    Arrancando con operadores

---
+++ aprendiendo-php/05-constantes/index.php
@@ -0,0 +1,22 @@
+<?php
+
+//Constantes
+
+define('nombre', 'Alvaro');
+define('apellido', 'Chagas');
+
+
+echo '<h1>'. apellido. '</h1>';
+echo '<h1>'. nombre. '</h1>';
+
+$apellido = 'Chagas Capurro';
+echo '<h1>'. $apellido. '</h1>';
+
+//Constantes predefinidas
+
+//echo phpinfo();
+
+echo PHP_OS;
+echo PHP_EXTENSION_DIR;
+
+    echo PHP__OS;
---
+++ aprendiendo-php/06-operadores/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//Operadores aritméticos
+
+$numero1 = 65;
+$numero2 = 33;
+
+echo 'Suma: ' .($numero1 + $numero2);
+echo '<br>';
+echo 'Resta: ' .($numero1 - $numero2);
+echo '<br>';
+echo 'Multiplicacion: ' .($numero1 * $numero2);
+echo '<br>';
+echo 'Division: ' .($numero1 / $numero2);
+echo '<br>';
+echo 'Resto: ' .($numero1 % $numero2);
